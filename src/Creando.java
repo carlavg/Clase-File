d07a538c7df71ba224fa7e923827eab213f61aad
carlavg <vgdaniela514@gmail.com>
2020-10-06 01:12:57 -0300

    Subida de proyecto

---
+++ src/Creando.java
@@ -0,0 +1,44 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Creando {
+
+	public static void main(String[] args) {
+
+		File ruta = new File("C:" + File.separator + "Users" + File.separator + "Juan" + File.separator + "Escritorio"
+				+ File.separator + "java-ser" + File.separator + "pruebaTexto.txt");
+
+		String archivoDestino = ruta.getAbsolutePath();
+		// ruta.mkdir();
+
+		try {
+			ruta.createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		Escribiendo accedeEs = new Escribiendo();
+		accedeEs.escribir(archivoDestino);
+
+	}
+}
+
+class Escribiendo {
+	public void escribir(String rutaArchivo) {
+		String frase = "Esto es un ejemplo. Espero que salga";
+
+		try {
+			FileWriter escritura = new FileWriter(rutaArchivo);
+			for (int i = 0; i < frase.length(); i++) {
+				escritura.write(frase.charAt(i));
+			}
+
+			escritura.close();
+
+		} catch (IOException e) {
+		}
+
+	}
+
+}
---
+++ src/PruebasRutas.java
@@ -0,0 +1,30 @@
+import java.io.File;
+
+public class PruebasRutas {
+
+	public static void main(String[] args) {
+		
+		File ruta=new File("C:" + File.separator + "Users" + File.separator  + File.separator + "Desktop" +File.pathSeparator + "java-ser"); 
+		System.out.println(ruta.getAbsolutePath());
+		String[] nombres_archivos=ruta.list();
+		
+		for (int i=0;i<nombres_archivos.length;i++) {
+			System.out.println(nombres_archivos[i]);
+			
+			File f=new File(ruta.getAbsolutePath(),nombres_archivos[i]);
+		
+			if(f.isDirectory()) {
+				String [] archivos_subcarpeta=f.list();
+				
+				for (int j=0;j<archivos_subcarpeta.length;j++) {
+			
+					System.out.println(archivos_subcarpeta[j]);
+					
+				
+				}
+		
+		}
+	}
+
+	}
+}
